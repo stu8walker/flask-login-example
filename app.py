2fe4f7742d598be14c72a984c287784934e4fb1c
Stuart Walker <stuartwalker485@gmail.com>
2020-01-24 16:37:32 +0000

    Base html templates setup

--- app.py
+++ app.py
@@ -45,15 +45,15 @@ class User(UserMixin, db.Model):
 # Routes
 @app.route('/', methods=['GET'])
 def index():
-    return "Index Page"
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return "Login Page"
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return "Register Page"
+    return render_template('register.html')
 
 @app.route('/dashboard', methods=['GET'])
 @login_required
